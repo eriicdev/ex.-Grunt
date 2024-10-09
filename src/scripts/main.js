5813cb3d7afb0c6a2759f59655a08cbc3175b5ac
Erik costa <ec5489976@gmail.com>
2024-10-09 09:44:22 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('botao').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+    })
+})
